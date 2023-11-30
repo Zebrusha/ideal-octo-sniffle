69d2d854b3e92ee766f21949967eef95bb2af17b
Anastasia Voronchikhina <zebrushaa@mail.ru>
2023-11-30 15:26:51 +0300

    ch src

--- src/main/java/RadioService.java
+++ src/main/java/RadioService.java
@@ -1,2 +1,73 @@
 public class RadioService {
+    public int curentRadioStation;
+    public int curentVolume;
+
+    public int getCurentVolume() {
+        return curentVolume;
+    }
+
+    public int getCurentRadioStation() {
+        return curentRadioStation;
+    }
+
+    public void setCurentRadioStation(int newCurentRadioStation) {
+        if (newCurentRadioStation < 0) {
+            curentRadioStation = 9;
+            return;
+        }
+        if (newCurentRadioStation > 9) {
+            curentRadioStation = 0;
+            return;
+        }
+        curentRadioStation = newCurentRadioStation;
+    }
+
+    public void setCurentClic(int b) {
+        if (b == 1) {
+            int target = curentRadioStation + 1;
+            setCurentRadioStation(target);
+        }
+        if (b == 0) {
+            int target = curentRadioStation - 1;
+            setCurentRadioStation(target);
+        }
+    }
+
+    public void setToMaxChanel() {
+        curentRadioStation = 9;
+    }
+
+
+    public void increaseRadioMax() {
+        int target = curentRadioStation + 1;
+        setCurentRadioStation(target);
+
+    }
+
+    public void increaseRadioMin() {
+        int target = curentRadioStation + 1;
+        setCurentRadioStation(target);
+    }
+
+    public void setCurentVolume(int newCurentVolume) {
+        if (newCurentVolume < 0) {
+            return;
+        }
+        if (newCurentVolume > 100) {
+            return;
+        }
+        curentVolume = newCurentVolume;
+    }
+
+    public void setCurentClicVolume(int b) {
+        if (b == 1) {
+            int target = curentVolume + 1;
+            setCurentVolume(target);
+        }
+        if (b == 0) {
+            int target = curentVolume - 1;
+            setCurentVolume(target);
+        }
+    }
+
 }
--- src/test/java/RadioServiceTest.java
+++ src/test/java/RadioServiceTest.java
@@ -1,2 +1,81 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RadioServiceTest {
+
+    @Test
+    public void NomberCurentStation() {
+        RadioService radio = new RadioService();
+
+        radio.setCurentRadioStation(7);
+
+        int expected = 7;
+        int actual = radio.getCurentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldIncreaseTestchanelMax() {
+        RadioService radio = new RadioService();
+        radio.setCurentRadioStation(9);
+        radio.setCurentClic(1);
+
+
+        int expected = 0;
+        int actual = radio.getCurentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseTestchanelMin() {
+        RadioService radio = new RadioService();
+        radio.setCurentRadioStation(0);
+        radio.setCurentClic(0);
+
+
+        int expected = 9;
+        int actual = radio.getCurentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void souldIncreaseTestVolume() {
+        RadioService radio = new RadioService();
+        radio.setCurentVolume(40);
+        radio.setCurentClicVolume(1);
+
+        int expected = 41;
+        int actual = radio.getCurentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void souldIncreaseTestVolumeMax() {
+        RadioService radio = new RadioService();
+        radio.setCurentVolume(100);
+        radio.setCurentClicVolume(1);
+
+        int expected = 100;
+        int actual = radio.getCurentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void souldIncreaseTestVolumeMin() {
+        RadioService radio = new RadioService();
+        radio.setCurentVolume(0);
+        radio.setCurentClicVolume(0);
+
+        int expected = 0;
+        int actual = radio.getCurentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
